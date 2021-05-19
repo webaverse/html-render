de482778f273d9ca619e8e4538ee9a9a636ed5ae
Avaer Kazmer <a@modules.io>
2021-05-19 15:13:03 -0400

    Update html-render-api.js

--- html-render-api.js
+++ html-render-api.js
@@ -11,6 +11,8 @@ class HtmlRenderer {
     this.iframe = iframe;
     document.body.appendChild(iframe);
     
+    this.ids = 0;
+    
     this.loadPromise = (async () => {
       await new Promise((accept, reject) => { 
         iframe.addEventListener('load', e => {
@@ -27,7 +29,7 @@ class HtmlRenderer {
   async waitForLoad() {
     return await this.loadPromise;
   }
-  render(htmlString, ) {
+  render(htmlString) {
     const {contentWindow} = this.iframe;
     
     const messageChannel = new MessageChannel();
@@ -50,10 +52,23 @@ class HtmlRenderer {
     messageChannel.port2.start();
     
     {
+      const id = ++this.ids;
+      const templateData = null;
+      const width = canvas.width;
+      const height = canvas.height;
+      const transparent = true;
+      const bitmap = null;
       const port = messageChannel.port1;
       // console.log('post message 1');
       contentWindow.postMessage({
         method: 'render',
+        id,
+        htmlString,
+        templateData,
+        width,
+        height,
+        transparent,
+        bitmap,
         port,
       }, '*', [port]);
       // console.log('post message 2');
