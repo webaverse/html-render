a7c6737de43a276f466fdad93b33a66b353c0b14
Avaer Kazmer <a@modules.io>
2021-05-19 01:23:34 -0400

    Update test.js

--- test.js
+++ test.js
@@ -7,6 +7,18 @@
     messageChannel.port2.addEventListener('message', e => {
       console.log('got result', e.data.error, e.data.result);
       const {error, result} = e.data;
+      
+      if (!error) {
+        const canvas = document.getElementById('canvas');
+        canvas.width = result.width;
+        canvas.height = result.height;
+        const ctx = canvas.getContext('2d');
+        const imageData = ctx.createImageData(result.width, result.height);
+        imageData.data.set(result.data);
+        ctx.putImageData(imageData, 0, 0);
+      } else {
+        throw new Error(error);
+      }
     });
     messageChannel.port2.start();
     
