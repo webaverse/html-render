f64304059fd03714007052f80e4202c87f2d7398
Avaer Kazmer <a@modules.io>
2021-05-19 15:09:41 -0400

    Update test to use new API

---
+++ html-render-api.js
@@ -0,0 +1,63 @@
+class HtmlRenderer {
+  constructor() {
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://127.0.0.1:2223'; // 'https://html-render.webaverse.com/';
+    iframe.style.cssText = `\
+      position: absolute;
+      top: -10000;
+      left: -10000;
+      visibility: hidden;
+    `;
+    this.iframe = iframe;
+    document.body.appendChild(iframe);
+    
+    this.loadPromise = (async () => {
+      await new Promise((accept, reject) => { 
+        iframe.addEventListener('load', e => {
+         accept();
+        });
+        iframe.addEventListener('error', reject);
+      });
+    })();
+  }
+  destroy() {
+    document.body.removeChild(this.iframe);
+    this.iframe = null;
+  }
+  async waitForLoad() {
+    return await this.loadPromise;
+  }
+  render(htmlString, ) {
+    const {contentWindow} = this.iframe;
+    
+    const messageChannel = new MessageChannel();
+    messageChannel.port2.addEventListener('message', e => {
+      console.log('got result', e.data.error, e.data.result);
+      const {error, result} = e.data;
+      
+      if (!error) {
+        const canvas = document.getElementById('canvas');
+        canvas.width = result.width;
+        canvas.height = result.height;
+        const ctx = canvas.getContext('2d');
+        const imageData = ctx.createImageData(result.width, result.height);
+        imageData.data.set(result.data);
+        ctx.putImageData(imageData, 0, 0);
+      } else {
+        throw new Error(error);
+      }
+    });
+    messageChannel.port2.start();
+    
+    {
+      const port = messageChannel.port1;
+      // console.log('post message 1');
+      contentWindow.postMessage({
+        method: 'render',
+        port,
+      }, '*', [port]);
+      // console.log('post message 2');
+    }
+  }
+}
+export default HtmlRenderer;
--- test.js
+++ test.js
@@ -1,35 +1,13 @@
+import HtmlRenderer from './html-render-api.js';
+
 (async () => {
-  const iframe = document.getElementById('iframe');
-  iframe.src = 'https://127.0.0.1:2223'; // 'https://html-render.webaverse.com/';
-  iframe.addEventListener('load', e => {
-    const {contentWindow} = iframe;
-    const messageChannel = new MessageChannel();
-    messageChannel.port2.addEventListener('message', e => {
-      console.log('got result', e.data.error, e.data.result);
-      const {error, result} = e.data;
-      
-      if (!error) {
-        const canvas = document.getElementById('canvas');
-        canvas.width = result.width;
-        canvas.height = result.height;
-        const ctx = canvas.getContext('2d');
-        const imageData = ctx.createImageData(result.width, result.height);
-        imageData.data.set(result.data);
-        ctx.putImageData(imageData, 0, 0);
-      } else {
-        throw new Error(error);
-      }
-    });
-    messageChannel.port2.start();
-    
-    {
-      const port = messageChannel.port1;
-      // console.log('post message 1');
-      contentWindow.postMessage({
-        method: 'render',
-        port,
-      }, '*', [port]);
-      // console.log('post message 2');
-    }
-  });
+  console.log('create renderer');
+  const htmlRenderer = new HtmlRenderer();
+  console.log('wait for load');
+  await htmlRenderer.waitForLoad();
+  console.log('render 1');
+  htmlRenderer.render(`\
+    <h1>Lollercopter</h1>
+  `);
+  console.log('render 2');
 })();
